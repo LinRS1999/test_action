7930c919908e3132a4c016e21eca590569d88a1a
LinRS1999 <623156869@qq.com>
2023-05-23 16:29:40 +0800

    update yaml

--- demo/test.py
+++ demo/test.py
@@ -1,9 +1,9 @@
 import datetime
 
-now = datetime.datetime.now()
+now = datetime.datetime.now() + datetime.timedelta(hours=8)
 current_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
-with open("nowtime.txt", "w") as f:
+with open("demo/nowtime.txt", "w") as f:
     f.write(current_time)
 
 
